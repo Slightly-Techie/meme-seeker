7c4503fd97de3539e89dcc34812c0ff340fa9ac0
TMCreme <tonnybright123@yahoo.com>
2023-02-11 17:15:42 +0000

    added the worker to listen for mentions

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # meme-seeker
+
+### Sample config.ini
+
+[twitter]
+consumer_key=xxx
+consumer_secret=xxxx
+access_token_default=xxxx
+access_secret_default=xxxxx
+access_token=xxx
+access_secret=xxx
+username=xxxxx
+
+[database]
+name=xxxx
+user=xxxxx
+password=xxxxx
+
--- worker.py
+++ worker.py
@@ -1,1 +1,39 @@
-# worker that listens for mentions goes here
+"""
+worker that listens for mentions goes here
+"""
+import json
+
+from tweepy import Stream
+
+from util.utils import load_config
+from util.logger_tool import Logger
+
+
+class StreamWorker(Stream):
+    """Main worker that streams and listens for mentions"""
+
+    def on_connect(self):
+        Logger.info("Connection established for Streaming")
+
+    def on_status(self, status):
+        Logger.info(status)
+
+    def on_data(self, raw_data):
+        json_data = json.loads(raw_data)
+        print(json_data)
+        Logger.info(raw_data)
+
+    def on_request_error(self, status_code):
+        print(status_code)
+        Logger.debug(status_code)
+
+
+if __name__ == "__main__":
+    username = [load_config("twitter", "username")]
+    stream = StreamWorker(
+        load_config("twitter", "consumer_key"),
+        load_config("twitter", "consumer_secret"),
+        load_config("twitter", "access_token_default"),
+        load_config("twitter", "access_secret_default")
+        )
+    stream.filter(track=username, threaded=True)
