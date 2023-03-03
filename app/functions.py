8df8d50acf664cdeebe5bedbc258920d29a02672
TMCreme <tonnybright123@yahoo.com>
2023-03-03 13:12:19 +0000

    functions added with util and logger

---
+++ app/functions.py
@@ -0,0 +1,105 @@
+"""
+Functions for various actions to perform on the image/gif/video
+"""
+from threading import Thread
+import mimetypes
+import shutil
+import uuid
+import boto3
+import requests
+import pika
+# from concurrent.futures import ThreadPoolExecutor
+from db.operations import DbOperations
+# from util.logger_tool import Logger
+from util.utils import load_config
+
+
+def consume_from_queue():
+    """Consume data from RabbitMQ Queue"""
+    conn = pika.BlockingConnection(
+        pika.ConnectionParameters(
+            "rabbitmq"
+        )
+    )
+    channel = conn.channel()
+    new_message = channel.basic_consume(
+        queue="produce_meme",
+        auto_ack=True
+        )
+    print(new_message)
+
+
+def download_image(data):
+    """get the image file from the url"""
+    media_url = data["entities"]["media"][0]["media_url"]
+    response = requests.get(media_url)
+    video_filename = None
+    if data["extended_entities"]["media"][0]["video_info"]:
+        video_url = data["extended_entities"]["media"][0][
+                    "video_info"
+                    ]["variants"][0]["url"]
+        video_filename = save_video(
+            video_url
+        )
+        video_thread = Thread(
+            target=push_to_s3, args=(video_filename,))
+        video_thread.start()
+        video_thread.join()
+
+    image_file = response.content
+    img_format = mimetypes.guess_extension(
+        response.headers.get('content-type').split(";")[0]
+    )
+    db_data = {
+        "image_blob": image_file,
+        "video_filename": video_filename,
+        "image_url": media_url,
+        "video_url": video_url,
+        "tweet_id": data["id_str"]
+    }
+
+    DbOperations().save_image(
+        db_data, img_format
+        )
+    return [data["id_str"], video_filename]
+
+
+def save_video(video_url):
+    """Save video to AWS S3 bucket"""
+    video_info_url = video_url
+    filename = uuid.uuid4().hex
+    video_res = requests.get(video_info_url, stream=True)
+    video_format = mimetypes.guess_extension(
+        video_res.headers.get('content-type').split(";")[0]
+    )
+
+    with open(filename+video_format, "wb") as f:
+        shutil.copyfileobj(video_res.raw, f)
+    video_res.close()
+
+    return filename+video_format
+
+
+def push_to_s3(filename):
+    """Push video/gif to S3"""
+    bucket_name = load_config(
+        "aws_credentials", "aws_storage_bucket"
+    )
+    s3_client = boto3.client(
+        "s3",
+        aws_access_key_id=load_config(
+            "aws_credentials", "aws_access_key"),
+        aws_secret_access_key=load_config(
+            "aws_credentials", "aws_secret_access_key"
+        )
+    )
+    s3_client.upload_file(
+        filename,
+        bucket_name,
+        filename
+    )
+    return
+
+
+def send_tweet():
+    """Tweet an image or gif"""
---
+++ app/util/utils.py
@@ -0,0 +1,33 @@
+"""
+The tiny stuff
+"""
+import json
+import sys
+import configparser
+from .logger_tool import Logger
+
+
+def load_config(section: str, name: str, file="config.ini"):
+    """Change config from json to ini for configparser"""
+    try:
+        file = "config.ini"
+        config = configparser.ConfigParser()
+        config.read(file)
+
+        return config[section][name]
+    except FileNotFoundError as file_err:
+        Logger.debug(file_err)
+        return None
+    except KeyError as key_err:
+        Logger.debug(key_err)
+        return None
+
+
+def save_config(data: dict, file: str):
+    with open(file, "w") as f:
+        json.dump(data, f)
+
+
+def print_err(err):
+    output = str(err) + " on line " + str(sys.exc_info()[2].tb_lineno)
+    Logger.fatal(output)
