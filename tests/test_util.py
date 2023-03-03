0d0ac22656488b135a8f8204942a92530d49b7d0
TMCreme <tonnybright123@yahoo.com>
2023-03-03 13:17:49 +0000

    test util to use pytest instead of unittest

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,37 +1,32 @@
 """
 Test Cases for the functinos in utils
 """
-import unittest
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-from util.utils import load_config
+import pytest
+from app.util.utils import load_config
 
 
-class UtilTestCases(unittest.TestCase):
-    """Test cases for functions"""
+def test_load_config_success():
+    """Test config and return item"""
+    config_result = load_config(
+        "twitter",
+        "username"
+    )
+    assert config_result is not None
 
-    def test_load_config_success(self):
-        """Test config and return item"""
-        config_result = load_config(
-            "twitter",
-            "username"
-        )
-        self.assertIsNotNone(config_result)
 
-    def test_load_config_no_section(self):
-        """Test section does not exist"""
+def test_load_config_no_section():
+    """Test section does not exist"""
 
-        config_result = load_config(
-            "instagram", "secret_key"
-        )
-        self.assertIsNone(config_result)
+    config_result = load_config(
+        "instagram", "secret_key"
+    )
+    assert config_result is None
 
-    def test_config_name_no_exist(self):
-        """Test section exists but object name no exist"""
 
-        config_result = load_config(
-            "twitter", "somethingelse"
-        )
-        self.assertIsNone(config_result)
+def test_config_name_no_exist():
+    """Test section exists but object name no exist"""
+
+    config_result = load_config(
+        "twitter", "somethingelse"
+    )
+    assert config_result is None
