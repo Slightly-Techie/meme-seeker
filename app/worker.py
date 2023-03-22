4fade8dd6510c7be38fc28e6cb27b54e6000d5fc
TMCreme <tonnybright123@yahoo.com>
2023-03-22 22:59:29 +0000

    changed stream worker to v2

--- app/worker.py
+++ app/worker.py
@@ -2,28 +2,32 @@
 worker that listens for mentions goes here
 """
 import json
-from tweepy import (
-    Stream,
-    API, OAuthHandler
-)
+from tweepy import StreamingClient, Client, StreamRule
 # import pika
+import requests
 from util.utils import load_config
 from util.logger_tool import Logger
 
 from functions import download_image
 
-
+bearer_token = load_config("twitter", "bearer_token")
 consumer_key = load_config("twitter", "consumer_key")
 consumer_secret = load_config("twitter", "consumer_secret")
 access_token = load_config("twitter", "access_token_default")
 access_token_secret = load_config("twitter", "access_secret_default")
+base_url = load_config("twitter", "base_url")
 
-v1_auth = OAuthHandler(consumer_key, consumer_secret)
-v1_auth.set_access_token(access_token, access_token_secret)
-v1_api = API(v1_auth)
+tweet_client = Client(
+    bearer_token=bearer_token,
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
+    wait_on_rate_limit=True
+    )
 
 
-class StreamWorker(Stream):
+class StreamWorker(StreamingClient):
     """Main worker that streams and listens for mentions"""
 
     def on_connect(self):
@@ -36,13 +40,14 @@ class StreamWorker(Stream):
         json_data = json.loads(raw_data)
         print(json_data)
         Logger.info(raw_data)
-        if "in_reply_to_status_id_str" in json_data:
+        # self.publish_to_queue(raw_data)
+        if "referenced_tweets" in json_data["data"]:
             tweet_data = self.get_orginal_tweet(
-                json_data["in_reply_to_status_id_str"]
+                json_data["data"]["referenced_tweets"][0]["id"]
             )
         else:
             tweet_data = json_data
-        response = download_image(tweet_data)
+        response = download_image(tweet_data, json_data["data"]["id"])
         if response is not None:
             send_sample_tweet(response)
 
@@ -52,20 +57,28 @@ class StreamWorker(Stream):
 
     def get_orginal_tweet(self, tweet_id):
         Logger.info(tweet_id)
-        original_tweet = v1_api.get_status(tweet_id)
-        Logger.info(json.dumps(original_tweet._json))
-        Logger.info(original_tweet.text)
-
-        return original_tweet._json
+        original_tweet_res = requests.get(
+            base_url + "/tweets/{}".format(tweet_id),
+            params={
+                "expansions": "attachments.media_keys,entities.mentions.username",
+                "tweet.fields": "entities",
+                "media.fields": "variants,preview_image_url"
+            },
+            headers={"Authorization": "Bearer {}".format(bearer_token)}
+        )
+        original_tweet = original_tweet_res.json()
+        Logger.info(original_tweet["data"])
+        Logger.info(json.dumps(original_tweet))
+        return original_tweet
 
 
 def send_sample_tweet(tweet_data):
     """Respond to tweet"""
     try:
-        v1_api.update_status(
-            "Done. Have a wonderful day.",
-            in_reply_to_status_id=tweet_data[0]
-        )
+        tweet_client.create_tweet(
+            text="Done. Have a wonderful day.",
+            in_reply_to_tweet_id=tweet_data[0],
+            )
         Logger.info("Tweet Response sent sucessfully")
     except Exception as tweeting_error:
         Logger.fatal(tweeting_error)
@@ -74,12 +87,25 @@ def send_sample_tweet(tweet_data):
 if __name__ == "__main__":
     username = load_config("twitter", "username")
     stream = StreamWorker(
-        consumer_key=consumer_key,
-        consumer_secret=consumer_secret,
-        access_token=access_token,
-        access_token_secret=access_token_secret,
+        bearer_token=bearer_token
         )
+    stream.add_rules(StreamRule(username))
     stream.filter(
-        track=[username],
+        expansions=[
+            "referenced_tweets.id",
+            "referenced_tweets.id.author_id",
+            "attachments.media_keys"
+        ],
+        user_fields=[
+            "entities",
+        ],
+        media_fields=[
+            "preview_image_url",
+            "variants"
+        ],
+        tweet_fields=[
+            "entities"
+        ],
         threaded=True
     )
+
