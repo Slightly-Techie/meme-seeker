3492a37af3370b84b34613648ec09b097c36d756
TMCreme <tonnybright123@yahoo.com>
2023-05-18 14:16:50 +0000

    image hash and read image file

--- app/functions.py
+++ app/functions.py
@@ -5,6 +5,7 @@ from threading import Thread
 import mimetypes
 import shutil
 import uuid
+import hashlib
 import boto3
 import requests
 from db.operations import DbOperations
@@ -12,12 +13,12 @@ from util.logger_tool import Logger
 from util.utils import load_config
 
 
-
-def download_image(data, tweet_id, username=""):
+def download_image(data, tweet_id, tweet_text):
     """get the image file from the url"""
     Logger.info("Media download in progress.....")
     if "media" in data["includes"]:
-        media_url = data["data"]["entities"]["urls"][0]["url"]
+        Logger.info("Found Media. Proceeding to download...")
+        media_url = data["includes"]["media"][0]["preview_image_url"]
         response = requests.get(media_url)
         video_filename = None
         video_url = None
@@ -35,30 +36,45 @@ def download_image(data, tweet_id, username=""):
         img_format = mimetypes.guess_extension(
             response.headers.get('content-type').split(";")[0]
         )
+        hash_digest = hash_file(image_file)
         db_data = {
             "image_blob": image_file,
+            "image_hash_digest": hash_digest,
             "video_filename": video_filename,
             "image_url": media_url,
             "video_url": video_url,
             "tweet_id": tweet_id
         }
 
-        DbOperations().save_image(
-            db_data, img_format, username=username
+        save_image_res = DbOperations().save_image(
+            db_data, img_format, keyword_tag="".join(tweet_text.split()[1:])
             )
+        if save_image_res == "Image exists":
+            return "Image exists"
         return [tweet_id, video_filename]
     else:
+        Logger.info("No media data found")
         pass
+
+
+def get_image_file(tweet_text):
+    """get the image blob and construct the image"""
+    print(tweet_text)
+    tweet_tag = "".join(tweet_text.split()[2])
+    image_blob = DbOperations().get_image(tweet_tag)
+    return image_blob
 
 
 def save_video(video_url):
     """Save video to AWS S3 bucket"""
+    Logger.info("Saving video as a file on local system")
     video_info_url = video_url
     filename = uuid.uuid4().hex
     video_res = requests.get(video_info_url, stream=True)
     video_format = mimetypes.guess_extension(
         video_res.headers.get('content-type').split(";")[0]
     )
+    Logger.info("Filename: {}".format(filename+video_format))
 
     with open(filename+video_format, "wb") as f:
         shutil.copyfileobj(video_res.raw, f)
@@ -80,9 +96,19 @@ def push_to_s3(filename):
             "aws_credentials", "aws_secret_access_key"
         )
     )
-    s3_client.upload_file(
+    response = s3_client.upload_file(
         filename,
         bucket_name,
         filename
     )
-    return
+    Logger.info("AWS S3 response: {}".format(response))
+    return True
+
+
+def hash_file(image_blob):
+    """Hash the image blob"""
+    Logger.info("Hash the blob and return the hash digest")
+    hasher = hashlib.sha256()
+    hasher.update(image_blob)
+    Logger.info("Hashing complete!!")
+    return hasher.hexdigest()
